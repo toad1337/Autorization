e5fb0e2ca741a7e7beb7fe78b2076a8d0ca25411
михаил <@DESKTOP-G5777UT>
2022-12-24 03:32:48 +0700

    Добавьте файлы проекта.

---
+++ авторизация/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace авторизация
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            form2 = new Form2();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+        Form2 form2;
+        string login = "12345";
+        string password = "12345";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string log = textBox1.Text;
+            string pass = textBox2.Text;
+            if(log == login && pass == password) 
+            {
+                form2.Show();
+
+            }
+            else 
+            {
+                MessageBox.Show("Неправильный логин или пароль", "ошибка авторизации");
+            }
+        }
+    }
+}
